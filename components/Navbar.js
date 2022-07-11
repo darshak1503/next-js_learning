b39364c9a47b3ac7b626a4b095b15e04d53459a6
darshak1503 <71271878+darshak1503@users.noreply.github.com>
2022-07-11 17:57:09 +0530

    Contact page done

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React from "react";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 const Navbar = () => {
   return (
-    <div>
- <nav className={styles.mainnav}>
-            <ul> 
-             <Link href='/'><a><li>Home</li></a></Link>
-             <Link href= '/about'><a><li>About</li></a></Link>
-             <Link href= '/blog'><a><li>Blogs</li></a></Link>
-             <Link href= '/contact'><a><li>Contacts</li></a></Link>
-            </ul>
-          </nav>
-
+    <div className={styles.mainnavbar}>
+      <nav className={styles.mainnav}>
+        <ul>
+          <Link href="/">
+            <a>
+              <li>Home</li>
+            </a>
+          </Link>
+          <Link href="/about">
+            <a>
+              <li>About</li>
+            </a>
+          </Link>
+          <Link href="/blog">
+            <a>
+              <li>Blogs</li>
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a>
+              <li>Contacts</li>
+            </a>
+          </Link>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
