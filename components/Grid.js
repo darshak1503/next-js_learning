104b511a32c47d060268bd1d7efff69e02894577
darshak1503 <71271878+darshak1503@users.noreply.github.com>
2022-07-14 12:15:53 +0530

    Basic grid done in home page

---
+++ components/Grid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Stylemain, Stylediv1 ,Stylediv2 ,Stylediv3, Stylediv4, Stylediv5 ,Stylediv6, Stylediv7} from './Style/Grid.styled'
+export default function Grid() {
+  return (
+    <Stylemain>
+        <Stylediv1>header</Stylediv1>
+        <Stylediv2>box1</Stylediv2>
+        <Stylediv3>box2</Stylediv3>
+        <Stylediv4>box3</Stylediv4>
+        <Stylediv5>siderbar</Stylediv5>
+        <Stylediv6>main content</Stylediv6>
+        <Stylediv7>footer</Stylediv7>
+    </Stylemain>
+  )
+}
